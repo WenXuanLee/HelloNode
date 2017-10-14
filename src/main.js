41b75bd3c24592c4a4efb3af86d0400ff4f3a84a
Wen Xuan Lee <speechless0922@gmail.com>
2017-10-14 12:32:47 +0800

    node module

---
+++ src/main.js
@@ -0,0 +1,12 @@
+// 告訴程式我要用lodash
+var _ = require('lodash');
+var m = require('./module.js');
+
+
+// Join lodash provided
+// 是把前面的Array接起來成為一個String,Array間
+// 用後面的元素接合
+console.log(_.join(['Hello', 'Node.js'],' '));
+console.log(m.name);
+console.log(m.version);
+m.f();
---
+++ src/module.js
@@ -0,0 +1,21 @@
+module.exports = {
+	name: 'Module',
+	version: 1.0,
+	f:function() {
+		console.log('Hello Module 2');
+	}
+};
+
+
+
+
+
+
+
+
+// exports.name = 'Module';
+// exports.version = 1.0;
+
+// exports.f = function() {
+// 	console.log('Hello module!');
+// };
